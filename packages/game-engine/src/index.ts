efe8db6e500b0e96158df52fef5ceabd2b7af1f8
tusanbot <mshmahyar9@gmail.com>
2026-09-26 01:27:11 +0330

    feat: add game engine interfaces

---
+++ packages/game-engine/src/index.ts
@@ -0,0 +1,14 @@
+export type PlayerId = string;
+
+export interface GamePlayer { id: PlayerId; seat: number; }
+
+export interface GameDefinition<State = unknown, Action = unknown> {
+  id: string;
+  minPlayers: number;
+  maxPlayers: number;
+  createState(players: GamePlayer[]): State;
+  applyAction(state: State, action: Action, playerId: PlayerId): State;
+  isFinished(state: State): boolean;
+}
+
+export interface GameResult { winners: PlayerId[]; scores: Record<PlayerId, number>; }
