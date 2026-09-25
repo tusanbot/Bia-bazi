70a04135e3e1171e976d7e5b524d558756796dc4
tusanbot <mshmahyar9@gmail.com>
2026-09-26 01:41:06 +0330

    Fix Telegram helper import path

--- apps/web/app/room/[roomId]/page.tsx
+++ apps/web/app/room/[roomId]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useCallback, useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import { initTelegram, telegramUser } from "../../../../lib/telegram";
+import { initTelegram, telegramUser } from "../../../lib/telegram";
 
 type RoomPlayer = {
   id: string;
